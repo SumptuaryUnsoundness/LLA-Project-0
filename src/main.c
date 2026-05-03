89a4e9a5d8065d28cb2f1c1b6a1e8e51b9e2c1e9
anon <asus-vivobook-0-devuan>
2026-05-03 16:59:28 +1200

    Sun 03 May 2026 16:59:28 NZST

--- src/main.c
+++ src/main.c
@@ -3,13 +3,19 @@
 #include <assert.h>
 
 int main() {
-  kv_t *db = kv_init(16);
+  kv_t *db = kv_init(1024);
   printf("%p\n", db);
   printf("%ld\n", db->capacity);
 
-  assert(db != NULL);
-  assert(db->capacity == 16);
-  assert(db->count == 0);
+  kv_put(db, "hehe", "haha");
+  kv_put(db, "hehe", "hoho");
+  kv_put(db, "lala", "test");
+
+  for (int i = 0; i < db->capacity; i++) {
+    if (db->entries[i].key) {
+      printf("[%d] %s: %s\n", i, db->entries[i].key, db->entries[i].value);
+    }
+  }
 
   kv_free(db);
   return 0;
